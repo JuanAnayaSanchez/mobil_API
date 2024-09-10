f75a388b9723a2b39ac56f3a52b5d1ac787275a7
JuanAnayaSanchez <167732186+JuanAnayaSanchez@users.noreply.github.com>
2024-09-09 22:37:42 -0500

    Creacion endpoint de reportes

--- index.php
+++ index.php
@@ -465,5 +465,27 @@
             echo json_encode($response);
         }
     }
+
+    if($_SERVER['REQUEST_METHOD'] === 'GET' && $url === '/mobil_API/SelectReport'){
+        try{
+
+            $stmt = $dbConn->prepare("CALL select_report()");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            // Interpretar el resultado
+            $result = $result ?? null; // Si no hay resultado, se asume falso (0)
+
+            $response = new APIResponse(200, 'Success', $result);
+            header('Content-Type: application/json');
+            echo json_encode($response);
+            
+        }catch (PDOException $e) {
+            http_response_code(500);
+            $response = new APIResponse(500, 'Internal Server Error', ['error' => $e->getMessage()]);
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }
+    }
     
 ?>
